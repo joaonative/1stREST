6388ced876669d6b905fc5d62529aaa76a8a8d66
joaonative <jestudos127@gmail.com>
2023-11-27 14:33:55 -0300

    main app

---
+++ src/index.ts
@@ -0,0 +1,37 @@
+import express from "express";
+import http from "http";
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
+import compression from "compression";
+import cors from "cors";
+import mongoose, { mongo } from "mongoose";
+
+import router from "./router";
+
+const app = express();
+
+app.use(
+  cors({
+    credentials: true,
+  })
+);
+
+app.use(compression());
+app.use(cookieParser());
+app.use(bodyParser.json());
+
+const server = http.createServer(app);
+const port = 8080;
+
+server.listen(port, () => {
+  console.log(`server running on port ${port}`);
+});
+
+const MONGO_URL =
+  "mongodb+srv://joao35434:majocafra@cluster0.ifewpbc.mongodb.net/?retryWrites=true&w=majority";
+
+mongoose.Promise = Promise;
+mongoose.connect(MONGO_URL);
+mongoose.connection.on("error", (error: Error) => console.log(error));
+
+app.use("/", router());
